4e3dbce8e7d82ba0bc06f5f8b1083c0fac0e4809
trung08012002 <phanngocanhyd@gmail.com>
2025-08-03 11:55:52 +0700

    add: port

--- index.js
+++ index.js
@@ -27,6 +27,5 @@ app.get("/api/reverse", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("🚀 Backend server listening on http://localhost:5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
